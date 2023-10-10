9973963a0d64a7a2d5a714ca3eb9516240b6f826
ChauhanRohan-RC <com.production.rc@gmail.com>
2023-10-10 10:27:55 +0530

    xchart bug fixed

--- out/artifacts/core_src_zip/function/definition/DomainAnimationDurationScalerI.java
+++
@@ -1,8 +0,0 @@
-package function.definition;
-
-public interface DomainAnimationDurationScalerI {
-
-    float getDomainAnimationDurationScale();
-
-    DomainAnimationDurationScalerI setDomainAnimationDurationScale(float animationDurationScale);
-}
